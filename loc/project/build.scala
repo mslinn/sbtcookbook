54bd8c041a7da3f49259a4ed6f18df78ebe01ad7
Mike Slinn <mslinn@micronauticsresearch.com>
2012-01-22 22:07:16 -0800

    Got Josh's suggestion to compile but it always gives an empty list as the result

--- loc/project/build.scala
+++ loc/project/build.scala
@@ -1,7 +1,13 @@
 import sbt._
 import Keys._
 
-object HelloBuild extends Build {
+/*
+In SBT type:
+
+     show loc
+
+*/
+object Loc extends Build {
   val hwsettings = Defaults.defaultSettings ++ Seq(
     organization := "Micronautics Research",
     name         := "loc",
@@ -11,26 +17,23 @@ object HelloBuild extends Build {
 
   val loc = TaskKey[Unit]("loc", "Displays the number of lines of code in the project")
 
-/* Thanks Josh! Will get back to this in a couple of days
-def line_count(file: File): Long =  IO.readLines(file).length   // NOTE: Horribly inefficient and doesn't ignore comments.
+/** Doesn't ignore comments. */
+def line_count(file: File): Long =  IO.readLines(file).length   
 
-    def sources_line_count(file: Seq[File]): Long = 
-      if(files.empty) 0
-      else  file.par.map(line_count).reduce(_+_)
+    def sources_line_count(files: Seq[File]): Long = 
+      if (files.isEmpty) 
+        0
+      else  
+        files.par.map(line_count).reduce(_+_)
 
-     val locTask = lock <<= (sources in Compile) map sources_line_count
+     val locTask = loc <<= (sources in Compile) map sources_line_count
 
-
-then in SBT type:
-
-     show loc*/
-
-  val locTask = loc := {
+  //val locTask = loc := {
     // need to learn how to read a file so this next line can print the number of lines
-    println((sources in Compile) map (s => s.last.getAbsolutePath).readFileSomehow.lines.map(_.length).sum)
+//    println((sources in Compile) map (s => s.last.getAbsolutePath).readFileSomehow.lines.map(_.length).sum)
 	//set TaskKey[Unit]("k") <<= (sources in Compile) map (s => println(s.last.getName))
     //println("This project has " + sourceFiles.length + " files containing source code")
-  }
+  //}
 
   lazy val project = Project("project", file ("."), settings = hwsettings ++ Seq(locTask))
 }
